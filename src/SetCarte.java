572337e87fbb673214cac9ae23fec08ff3e1304e
Thomas Robson <twprobson@gmail.com>
2023-05-24 14:58:32 +0200

    feat(doc): settings schem

---
+++ src/SetCarte.java
@@ -0,0 +1,12 @@
+package idee_projet;
+
+import java.util.ArrayList;
+
+public class SetCarte {
+    private ArrayList<Carte> cartes;
+    private Image verso;
+
+    public void setVerso(Image verso) {
+        this.verso = verso;
+    }
+}
