f95a0d4e60e329e5cbb31ba2d063dd2c3749287b
Florian Soler <solerflorian@gmail.com>
2023-05-31 10:09:14 +0200

    feat(cardbutton): added card button ui.

---
+++ src/ButtonCard.java
@@ -0,0 +1,37 @@
+import javax.swing.*;
+
+public class ButtonCard extends JButton{
+
+    //Parameter
+    private int buttonId;
+    private boolean isVerso = false;
+
+    //card
+    private Card card;
+    private Icon cardRecto;
+
+    public ButtonCard(int buttonid, Card card, Icon rectoIcon){
+        this.buttonId = buttonid;
+        this.card = card;
+        this.cardRecto = rectoIcon;
+        initButtonGui();
+    }
+
+    private void initButtonGui(){
+        this.setIcon(this.cardRecto);
+        this.setContentAreaFilled(false);
+    }
+
+    public void fliped(){
+        this.isVerso = true;
+        this.setIcon(card.getCardImage());
+    }
+
+    public Card getCard() {
+        return card;
+    }
+
+    public int getButtonId() {
+        return buttonId;
+    }
+}
