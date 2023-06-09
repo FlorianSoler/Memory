e25af5ea2ec5595dabaa99c674f9cca6e8635864
Thomas Robson <twprobson@gmail.com>
2023-06-09 12:01:15 +0200

    feat(Score): addition off the ScorePannel

--- src/plateau_tools/Score.java
+++ src/plateau_tools/Score.java
@@ -1,4 +1,5 @@
 package plateau_tools;
+import frames.ScorePannel;
 
 public class Score {
     private int scoreJ1;
@@ -6,6 +7,7 @@ public class Score {
     private String pseudoJ1;
     private String pseudoJ2;
     private boolean isJ1CurrentPlayer;
+    private ScorePannel scorePannel;
 
     public Score(String pseudoJ1, String pseudoJ2){
         this.pseudoJ1 = pseudoJ1;
@@ -13,21 +15,24 @@ public class Score {
         this.scoreJ1 = 0;
         this.scoreJ2 = 0;
         this.isJ1CurrentPlayer = true;
+        this.scorePannel = new ScorePannel(pseudoJ1, pseudoJ2);
         
     }
 
     public void switchPlayer() {
         this.isJ1CurrentPlayer = ! this.isJ1CurrentPlayer;
+        this.scorePannel.switchPlayer();
     }
 
-    public int addPointJ1() {
-        this.scoreJ1 = this.scoreJ1 +1;
-        return this.scoreJ1;
-    }
-
-    public int addPointJ2() {
-        this.scoreJ2 = this.scoreJ2 +1;
-        return this.scoreJ2;
+    public void addPoint() {
+        if (this.isJ1CurrentPlayer) {
+            this.scoreJ1 = this.scoreJ1 +1;
+            this.scorePannel.addScoreJ1(this.scoreJ1);
+        } else {
+            this.scoreJ2 = this.scoreJ2 +1;
+            this.scorePannel.addScoreJ2(this.scoreJ2);
+        }
+        
     }
 
     /**
@@ -42,5 +47,26 @@ public class Score {
      */
     public String getPseudoJ2() {
         return pseudoJ2;
+    }
+
+    /**
+     * @return the scoreJ1
+     */
+    public int getScoreJ1() {
+        return scoreJ1;
+    }
+
+    /**
+     * @return the scoreJ2
+     */
+    public int getScoreJ2() {
+        return scoreJ2;
+    }
+
+    /**
+     * @return the scorePannel
+     */
+    public ScorePannel getScorePannel() {
+        return scorePannel;
     }
 }
